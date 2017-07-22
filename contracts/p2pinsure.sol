c4b4f136a03330e090c4b1bceaa4aa978f9f695e
David Mark <david.ike.mark@gmail.com>
2017-07-22 13:06:42 +0100

    p2pins

---
+++ contracts/p2pinsure.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.11;
+
+contract policy {
+    unit public premium;
+    address public adjudicator;
+    mapping(address => bool)  members;
+  
+    function create(unit _premium, address _adjudicator) {
+        premium = _premium;
+        adjudicator = _adjudicator
+    }
+    
+    function adjudicate(){}
+    
+    function join(){
+       person = msg.sender;
+       
+       if(msg.sender != adjudicator && !members[person]){
+            members[person] = true;
+        }
+       
+       // what if person already
+    }
+    
+    function claim(){
+    }
+    
+    function payout(){
+    
+    }
+    
+    function isMember()
+        returns (bool isMember)
+    {
+        if(members[person])
+            return true;
+    }
+}
