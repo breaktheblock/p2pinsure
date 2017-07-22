ce653b943aed008edefd33a347bae29e976bd055
Edward Ward <edwardwardward@gmail.com>
2017-07-22 15:14:13 +0100

    payout function added

--- contracts/p2pinsure.sol
+++ contracts/p2pinsure.sol
@@ -5,45 +5,39 @@ contract P2pinsure {
     address public adjudicator;
     mapping(address => uint)  members;
     uint totalValue;
-    
-  
+
+
     function create(uint _premium, address _adjudicator) {
         premium = _premium;
         adjudicator = _adjudicator;
     }
-    
-    function adjudicate(){}
-    
-    function join() payable{
-              // replace addr with msg.sender
-       address person = msg.sender;
 
-       if(person != adjudicator && members[person] != 0){
-            
-        }
-       
-       // what if person already
+    function adjudicate(){}
+
+    function join() payable {
+       address person = msg.sender;
+       require(person != adjudicator
+                && members[person] != 0);
+
     }
-    
+
     function claim(){
        address person = msg.sender;
        //address person = addr;
-       person.transfer(1);
+       person.transfer(1000000000000000000);
        if(members[person] != 0){
-           
-           
        }
     }
-    
+
     function payout(){
-    
+
     }
-    
+
     function isMember()
         returns (bool isMember)
     {
        var person = msg.sender;
-           
+
         if(members[person] != 0)
             return true;
     }
