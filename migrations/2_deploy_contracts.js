ebb9aafa09dfd6e57a440fb2c1cfe61f51cb55c4
David Mark <david.ike.mark@gmail.com>
2017-07-23 13:09:07 +0100

    final commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-var SimpleAuction = artifacts.require("./simpleAuction.sol");
 var P2pinsure = artifacts.require("./p2pinsure.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-  deployer.deploy(SimpleAuction);
-  deployer.deploy(P2pinsure);
+  deployer.deploy(P2pinsure, "0x9fa0637fea09f348b1f4f6b7b8650f1fb3a3e34d", false,"MyMutual",10,1);
 };
