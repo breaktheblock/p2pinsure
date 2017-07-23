aed4a469bb63c9607c0f238f6595ec58490f3726
Edward Ward <edwardwardward@gmail.com>
2017-07-23 11:18:02 +0100

    update contract

--- contracts/p2pinsure.sol
+++ contracts/p2pinsure.sol
@@ -4,26 +4,39 @@ contract P2pinsure {
     uint public premium;
     uint public quorumMultiplier;
     address public adjudicator;
+    address public owner;
     mapping(address => uint)  members;
     uint public memberCount;
-    mapping(address => uint)  claims;
+    mapping(address => uint64)  claims;
     mapping(address => mapping(address => bool)) claimantVoterMap;
+    mapping(address => bool) public joinRequests;
     bool public voting;
     uint public totalValue;
     mapping(address => uint) votesForClaim;
+    mapping(address => bool) whitelist;
     string public policyName;
+    uint public maxPoolSize;
+    // joingCriteria 1 == Open, 2 == Voting, 3 == Owner, Anyother number is  voting not implemented yet
+    uint public joiningCriteria;
 
 
     event Claim(address claimant, uint amount);
     event Vote(address claimant, address voter, bool choice);
     event Payout(address claimant, uint amount);
 
-    function create(address _adjudicator, bool _voting, string _policyName) {
+
+//
+    function P2pinsure(address _adjudicator, bool _isVotingResolutionMethod,
+                    string _policyName, uint _maxPoolSize){
+        // _joiningCriteria - open, owner approval, voting
+        // max pool size
+        owner = msg.sender;
         premium = 10;
         adjudicator = _adjudicator;
-        voting = _voting;
+        voting = _isVotingResolutionMethod;
         quorumMultiplier = 2;
         policyName = _policyName;
+        maxPoolSize = _maxPoolSize;
     }
 
     function adjudicate(address claimant, bool approve, uint negative_adjustment){
@@ -34,17 +47,26 @@ contract P2pinsure {
         }
     }
 
-    function join() payable {
+
+    function ownerWhitelist(address requestingAddress) {
+        if(joiningCriteria == 1) {
+            whitelist[requestingAddress] = true;
+        }
+
+    }
+
+    function join() payable whiteListCheck(msg.sender) {
        address person = msg.sender;
        require(person != adjudicator
                 && members[person] == 0
-                && msg.value > premium);
+                && msg.value > premium
+                && memberCount < maxPoolSize);
        members[person] = msg.value;
        totalValue += msg.value;
        memberCount += 1;
     }
 
-    function claim(uint amount){
+    function claim(uint64 amount){
        address person = msg.sender;
        require(members[person] > 0);
        claims[person] = amount;
@@ -54,7 +76,7 @@ contract P2pinsure {
     function vote(address claimant){
         require(voting == true && claimantVoterMap[claimant][msg.sender] != true);
         votesForClaim[msg.sender] += 1;
-        claimant_voter_map[claimant][msg.sender] = true;
+        claimantVoterMap[claimant][msg.sender] = true;
     }
 
     function check_payout(){
@@ -69,4 +91,12 @@ contract P2pinsure {
         if(members[person] != 0)
             return true;
     }
+
+    modifier whiteListCheck(address requestingMember) {
+        if(joiningCriteria != 1) {
+        require(whitelist[requestingMember] == true);
+        }
+        _;
+    }
+
 }
